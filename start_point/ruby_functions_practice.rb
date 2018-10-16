e21257f600e515b3b92f6c6810574705ceb9f4e8
Steven Kay <stevendkay@gmail.com>
2018-10-16 16:15:49 +0100

    4 new functions and tests passing

--- start_point/ruby_functions_practice.rb
+++ start_point/ruby_functions_practice.rb
@@ -10,6 +10,18 @@ def subtract(num1, num2)
   return num1.to_f - num2.to_f
 end
 
+def length_of_string(mystring)
+  return mystring.length
+end
+
+def join_string(string1, string2)
+  return string1 + string2
+end
+
+def add_string_as_number(numberstring1, numberstring2)
+  return numberstring1.to_i + numberstring2.to_i
+end
+
 def multiply(num1, num2)
   return num1.to_f * num2.to_f
 end
