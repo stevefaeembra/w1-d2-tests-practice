86664cfc20b74fcda0f7e424436d4080e6448a77
Malcolm Finlayson <malcolmfinlayson81@gmail.com>
2018-10-16 16:05:57 +0100

    correction -  ruby_functions_practice.rb now added

--- start_point/ruby_functions_practice.rb
+++ start_point/ruby_functions_practice.rb
@@ -5,3 +5,15 @@ end
 def add(num1, num2)
   return num1.to_f + num2.to_f
 end
+
+def subtract(num1, num2)
+  return num1.to_f - num2.to_f
+end
+
+def multiply(num1, num2)
+  return num1.to_f * num2.to_f
+end
+
+def divide(num1, num2)
+  return num1.to_f / num2.to_f
+end
